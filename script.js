37c8657cb76b1ccc65443b583d323f3348cab227
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-24 18:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+  window.addEventListener("resize" , updateSize);
+
+function updateSize(){
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	let changeSize= document.getElementById("sizeInfo");
+	changeSize.textContent = `Width: ${width} and Height: ${height}`;
+}
+updateSize();
